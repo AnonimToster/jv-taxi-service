b61f25fcb9b583ab138aafae01e93a4ef01ac50c
Danilo Kozakov <45821252+AnonimToster@users.noreply.github.com>
2023-04-11 19:54:41 +0300

    Readme

--- src/main/java/taxi/controller/manufacturer/CreateManufacturerController.java
+++ src/main/java/taxi/controller/manufacturer/CreateManufacturerController.java
@@ -2,7 +2,6 @@ package taxi.controller.manufacturer;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +9,7 @@ import taxi.lib.Injector;
 import taxi.model.Manufacturer;
 import taxi.service.ManufacturerService;
 
-public class CreateManufacturersController extends HttpServlet {
+public class CreateManufacturerController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private final ManufacturerService manufacturerService
             = (ManufacturerService) injector.getInstance(ManufacturerService.class);
